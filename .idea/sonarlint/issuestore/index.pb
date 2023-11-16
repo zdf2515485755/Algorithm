34d29ae8a4a6290459c81a8ae84d4cf681561be8
mrzhang <zdf2515485755@gmail.com>
2023-11-16 20:51:17 +0800

    完成了冒泡排序算法，选择排序算法，插入排序算法

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,22 @@
+
+O
+src/main/java/org/zdf/Main.java,0/b/0b54aac48a86fad32c97964d52e1b11b31061413
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+U
+%src/main/java/sort/SortAlgorithm.java,a/b/abad4ef8290c287cd1d517f292abcf6404b7065c
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+w
+G.idea/sonarlint/issuestore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,8/6/863e0292fd7e9b3e74dafd6cc01f73ba76346482
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
+Å
+Q.idea/sonarlint/securityhotspotstore/0/b/0b54aac48a86fad32c97964d52e1b11b31061413,4/8/484068d06b079dc958ac2d7670d3507208f6b9e8
+Å
+Q.idea/sonarlint/securityhotspotstore/4/4/442292b8a7efeabbe4cc176709b833b1792140ec,2/c/2cabb939da04d3de3a200bf2b575690e1a16316d
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
---
+++ src/main/java/sort/SortAlgorithm.java
@@ -0,0 +1,114 @@
+package sort;
+
+public class SortAlgorithm
+{
+    /**
+     * 用于交换数组位置index1和数组位置index2的方法
+     * @param index1
+     * @param index2
+     * @param array
+     */
+    public static void swap(int index1, int index2, int[] array)
+    {
+        //临时变量
+        int temporary = 0;
+        temporary = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temporary;
+    }
+
+    /**
+     * 判断数组是否有效
+     * @param array
+     */
+    public static boolean judgeArray(int[] array)
+    {
+        if (null == array || array.length == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+    }
+    /**
+     * 选择排序(从小到大)
+     * @param array
+     */
+    public static void selectSort(int[] array)
+    {
+        if (judgeArray(array))
+        {
+            return;
+        }
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            for (int j = i + 1; j < array.length; j++)
+            {
+                if (array[j] < array[i])
+                {
+                    swap(j, i, array);
+                }
+            }
+        }
+    }
+
+    /**
+     * 冒泡算法
+     * @param array
+     */
+    public static void bubbleSort(int[] array)
+    {
+        if (judgeArray(array))
+        {
+            return;
+        }
+
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            for (int j = 0; j < array.length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    swap(j, j + 1, array);
+                }
+            }
+
+        }
+    }
+
+    /**
+     * 插入排序
+     * @param array
+     */
+    public static void insertSort(int[] array)
+    {
+        if (judgeArray(array))
+        {
+            return;
+        }
+
+        for (int i = 1; i < array.length; i++)
+        {
+            for (int j = i; j != 0 && array[j] < array[j - 1]; j--)
+            {
+                swap(j, j - 1, array);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] arrays = {1,2,3};
+        //selectSort(arrays);
+        //bubbleSort(arrays);
+        insertSort(arrays);
+        for (int value:arrays) {
+            System.out.println(value);
+
+        }
+
+    }
+}
